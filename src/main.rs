8af47daaec36ccf03b9fea02e14198e878ced2f7
Yutaro Ohno <yutaro.ono.418@gmail.com>
2023-10-14 17:42:09 +0900

    Save output as an image file

--- src/main.rs
+++ src/main.rs
@@ -36,9 +36,21 @@ fn main() {
     let dom = html::parse(html.to_string());
     let cssom = css::parse(css.to_string());
     let style = style::style_tree(&dom, &cssom);
-    let mut viewport: layout::Dimensions = Default::default();
-    let layout = layout::layout_tree(&style, &mut viewport);
-    let canvas = painting::paint(&layout, 1024, 1024);
 
-    dbg!(canvas);
+    // Since we don't have an actual window, hard-code the "viewport" size.
+    let mut viewport: layout::Dimensions = Default::default();
+    viewport.content.width = 800.0;
+    viewport.content.height = 600.0;
+
+    let layout = layout::layout_tree(&style, &mut viewport);
+    let canvas = painting::paint(&layout, 800, 600);
+
+    // Save output as an image file.
+    // let file = File::create(&Path::new("output.png")).unwrap();
+    let (w, h) = (canvas.width as u32, canvas.height as u32);
+    let buffer: Vec<image::Rgba<u8>> = unsafe { std::mem::transmute(canvas.pixels) };
+    let img = image::ImageBuffer::from_fn(w, h, |x, y| {
+        buffer[(y * w + x) as usize]
+    });
+    image::DynamicImage::ImageRgba8(img).save("output.png").unwrap();
 }
